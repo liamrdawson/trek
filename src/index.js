f2fd1e04c34f5c46f514b7368bd3afce395cc495
Liam Dawson <liam.dawson@15gifts.com>
2020-02-14 17:12:20 +0000

    quick update to index.js with createStore

--- src/index.js
+++ src/index.js
@@ -1,8 +1,13 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { createStore } from 'redux';
 import './index.css';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
+
+const store = createStore(
+    
+);
 
 ReactDOM.render(<App />, document.getElementById('root'));
 
